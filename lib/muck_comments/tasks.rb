4d0abaad5016f2c8bfe238418cf262b2cb57128c
Justin Ball <justinball@gmail.com>
2010-02-17 15:54:46 -0700

    updated translations.  refactored tasks

--- lib/muck_comments/tasks.rb
+++ lib/muck_comments/tasks.rb
@@ -12,9 +12,9 @@ module MuckComments
     def define
       
       namespace :muck do
-        namespace :comments do
+        namespace :sync do
           desc "Sync required files from muck comments."
-          task :sync do
+          task :comments do
             path = File.join(File.dirname(__FILE__), *%w[.. ..])
             system "rsync -ruv #{path}/db ."
             #system "rsync -ruv #{path}/public ."
